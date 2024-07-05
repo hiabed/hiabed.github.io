6e5043c6c84b39b4c1e62dc3ccbd90cdd7c6ccef
Mohammed-Abed Hassani-Idrissi <mhassani@c3r3p2.1337.ma>
2024-07-05 22:41:43 +0100

    login

---
+++ login/script.js
@@ -0,0 +1,22 @@
+const singUp = document.querySelector("#signup");
+const singIn = document.querySelector("#signin")
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+
+function singUp_function(event)
+{
+    event.preventDefault();
+    one.style.display = "none";
+    three.style.display = "block";
+}
+
+function singIn_function(event)
+{
+    event.preventDefault();
+    three.style.display = "none";
+    one.style.display = "block";
+}
+
+singUp.addEventListener("click", singUp_function);
+singIn.addEventListener("click", singIn_function);

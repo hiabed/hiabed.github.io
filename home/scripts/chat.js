b079de3296cb5c66de2a725aa2705b61f40fb449
Mohammed-Abed Hassani-Idrissi <mhassani@e1r12p9.1337.ma>
2024-08-23 16:55:06 +0100

    some updates

--- home/scripts/chat.js
+++ home/scripts/chat.js
@@ -11,47 +11,47 @@ const chatFunction = () => {
 
 chatButton.addEventListener("click", chatFunction);
 
-// const data_example = async() => { // get characters.
-//     try {
-//         const data = await fetch("https://dattebayo-api.onrender.com/characters");
-//         if (data.ok)
-//         {
-//             const json_data = await data.json();
-//             return json_data.characters;
-//         }
-//     } catch (err) {
-//         console.error(err);
-//     }
-// }
+const data_example = async() => { // get characters.
+    try {
+        const data = await fetch("https://dattebayo-api.onrender.com/characters");
+        if (data.ok)
+        {
+            const json_data = await data.json();
+            return json_data.characters;
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
 
-// const container = document.querySelector("#msgs");
+const container = document.querySelector("#msgs");
 
-// const scrollToBottom = ()=> {
-//     container.scrollTop = container.scrollHeight;
-// }
+const scrollToBottom = ()=> {
+    container.scrollTop = container.scrollHeight;
+}
 
-// const data_characters = async() => {
-//     const characters = await data_example();
-//     characters.forEach(character => {
-//         const user = document.createElement("div");
-//         document.querySelector("#chats").appendChild(user);
-//         user.innerHTML = character.name;
-//         // document.querySelector("#chats div").style.border = "none";
-//         user.addEventListener("click", ()=> {
-//             const users = document.querySelectorAll("#chats div");
-//             users.forEach(userr => {
-//                 userr.classList.remove("selected-user");
-//                 userr.style.width = "90%";
-//                 userr.style.boxShadow = "0 0 5px #0e2c2e";
-//             })
-//             user.style.width = "95%";
-//             user.style.boxShadow = "0 0 5px #9bf9ff";
-//             user.classList.add("selected-user");
-//             document.querySelector("#chat-pic").style.backgroundImage  = `url("${character.images[0]}")`;
-//             document.querySelector("#secondd h3").innerHTML = character.name;
-//         })
-//     })
-// }
+const data_characters = async() => {
+    const characters = await data_example();
+    characters.forEach(character => {
+        const user = document.createElement("div");
+        document.querySelector("#chats").appendChild(user);
+        user.innerHTML = character.name;
+        // document.querySelector("#chats div").style.border = "none";
+        user.addEventListener("click", ()=> {
+            const users = document.querySelectorAll("#chats div");
+            users.forEach(userr => {
+                userr.classList.remove("selected-user");
+                userr.style.width = "90%";
+                userr.style.boxShadow = "0 0 5px #0e2c2e";
+            })
+            user.style.width = "95%";
+            user.style.boxShadow = "0 0 5px #9bf9ff";
+            user.classList.add("selected-user");
+            document.querySelector("#chat-pic").style.backgroundImage  = `url("${character.images[0]}")`;
+            document.querySelector("#secondd h3").innerHTML = character.name;
+        })
+    })
+}
 
 const sendMsg = document.querySelector("#something");
 
